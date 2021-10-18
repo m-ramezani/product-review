5c95ffe87753d8afe4a6968ffa16ad29741ab3d2
m-ramezani <53174070+m-ramezani@users.noreply.github.com>
2021-10-18 10:09:43 +0330

    Delete extra directory

--- mapper/ReviewMapper.java
+++
@@ -1,9 +0,0 @@
-package com.myshop.productreview.mapper;
-
-import com.myshop.productreview.dto.ReviewDto;
-import com.myshop.productreview.model.Review;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ReviewMapper extends EntityMapper<ReviewDto, Review> {
-}
